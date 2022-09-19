103dacd173e8fab856d9ab2fa5584ca444268460
wei-gao-2022 <wei.gao@sira.nsw.gov.au>
2022-09-19 13:00:24 +1000

    rename order card

---
+++ README.md
@@ -0,0 +1,6 @@
+## Assumptions:
+1. one account has at most one primary contact which can be selected during the lwc contact selection.
+2. primary contact is required during the order creation.
+
+## note
+a new customer field Primary_Contact__c was created to link the account and corresponding primary contact.
--- force-app/main/default/lwc/contactCreator/contactCreator.js
+++ force-app/main/default/lwc/contactCreator/contactCreator.js
@@ -3,27 +3,21 @@
  */
 
 import {LightningElement, api} from 'lwc';
-import saveContactAction from '@salesforce/apex/ContactController.saveContact';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ContactCreator extends LightningElement {
     @api recordId;
 
-    async handleNewContact(event){
-        event.preventDefault();
-
-        let conObj = {};
-        [...this.template.querySelectorAll('lightning-input-field')].forEach(item => {
-            conObj[item.dataset.name] = item.value;
-        });
-
-        console.log("conObj", conObj);
-
-        try{
-            await saveContactAction({acctId: this.recordId, conObj});
-        }catch(ex){
-            console.log(ex);
-        }
-
+    handleContactCreated(){
+        this.dispatchEvent(
+            new ShowToastEvent({
+                title: 'Success',
+                message: 'Contact created',
+                variant: 'success',
+            }),
+        );
+    
+        [...this.template.querySelectorAll(".editable")].forEach(item => item.reset());
     }
 
 }
